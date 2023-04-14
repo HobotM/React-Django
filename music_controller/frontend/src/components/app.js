98c4886328b5e5aa4ebf84d629c2e9b57e141b7a
HobotM <mateuszhobot@gmail.com>
2023-04-14 23:58:05 +0100

    added frontend react

---
+++ music_controller/frontend/src/components/app.js
@@ -0,0 +1,15 @@
+import React, {component} from "react";
+import {render} from "react-dom";
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return
+        <h1>Testing react app</h1>;
+    }
+
+}
