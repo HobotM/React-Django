fc758be51912d723de0545807205da6db3cdf46f
HobotM <mateuszhobot@gmail.com>
2023-04-17 23:36:44 +0100

    functionality for create room

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -16,7 +16,39 @@ export default class CreateRoomJoinPage extends Component {
     defaultVotes = 2;
     constructor(props) {
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        };
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
 
+    }
+
+    handleVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e){
+        this.setState({
+            guestCanPause:e.target.value === 'true' ? true : false,
+        });
+    }
+
+    handleRoomButtonPressed(){
+        const requestOptions = {
+            method: 'POST',
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            }),
+        };
+        fetch("/create-room", requestOptions).then((response) =>
+        response.json()).then((data)=> console.log(data));
     }
 
     render() {
@@ -31,10 +63,10 @@ export default class CreateRoomJoinPage extends Component {
                     <FormControl component="fieldset">
                         <FormHelperText>
                             <div align="center">
-                                Guest Control of PLayback State
+                                Guest Control of Playback State
                             </div>
                         </FormHelperText>
-                        <RadioGroup row defaultValue="true">
+                        <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
                             <FormControlLabel value="true"
                                 control={<Radio color="primary" />}
                                 label="Play/Pause"
@@ -50,7 +82,12 @@ export default class CreateRoomJoinPage extends Component {
                 </Grid>
                 <Grid item xs={12} align="center">
                     <FormControl>
-                        <TextField required={true} type="number" defaultValue={this.defaultVotes} inputProps={{ min: 1, style:{textAlign: "center"} }} />
+                        <TextField 
+                        required={true} 
+                        type="number" 
+                        onChange={this.handleVotesChange}
+                        defaultValue={this.defaultVotes} 
+                        inputProps={{ min: 1, style:{textAlign: "center"} }} />
                     <FormHelperText>
                         <div align="center">
                             Votes required to skip song
@@ -59,7 +96,13 @@ export default class CreateRoomJoinPage extends Component {
                     </FormControl>
                 </Grid>
                 <Grid item xs={12} align="center">
-                    <Button color="primary" variant="contained">Create A Room</Button>
+                    <Button color="primary" 
+                    variant="contained"
+                    onClick={this.handleRoomButtonPressed}
+                    
+                    >
+                        Create A Room
+                        </Button>
                 </Grid>
                 <Grid item xs={12} align="center">
                     <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
