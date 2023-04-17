bb4411041cf75b1f5d9fc5feb254b54039953f3d
HobotM <mateuszhobot@gmail.com>
2023-04-17 10:56:39 +0100

    Create a room page - no logic yet just a react

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -2,27 +2,22 @@ from django.db import models
 import string
 import random
 
-#function generates a unique alphanumeric code of length 6.
+
 def generate_unique_code():
     length = 6
 
     while True:
-        #This line of code generates a string of length length consisting of random uppercase letters
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        # The Room object's filter() method is used to check if any Room object in the database already has this code.
         if Room.objects.filter(code=code).count() == 0:
             break
-        return code
 
-# Create your models here.
+    return code
 
-# Room model
+
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    votes_to_skip = models.IntegerField(null=False, default=2)
-    created_at = models.DateField(auto_now_add=True)
-
-
-#Methods
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
