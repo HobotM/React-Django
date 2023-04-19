0f91ffd01d9f89eccdad21ad62a63f04e0632ca4
HobotM <mateuszhobot@gmail.com>
2023-04-19 11:43:11 +0100

    frontend for join page

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,4 @@
+# Importing the required packages and modules
 from django.shortcuts import render
 from rest_framework import generics, status
 from .serializers import RoomSerializer, CreateRoomSerializer
@@ -6,35 +7,39 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# Create your views here.
-
-
+# Class-based view to return a list of all the rooms
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
 
+# Class-based view to get a room with a given code from the database
 class GetRoom(APIView):
     serializer_class = RoomSerializer
     lookup_url_kwarg = 'code'
 
     def get(self, request, format=None):
+        # Fetch the room with the given code from the database
         code = request.GET.get(self.lookup_url_kwarg)
         if code != None:
             room = Room.objects.filter(code=code)
+            # If room exists with given code, then return its details
             if len(room) > 0:
                 data = RoomSerializer(room[0]).data
                 data['is_host'] = self.request.session.session_key == room[0].host
                 return Response(data, status=status.HTTP_200_OK)
+            # If room does not exist with given code, then return an error message
             return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
 
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Class-based view to create a new room with the provided data
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
     def post(self, request, format=None):
+        # Create a new session if one does not already exist
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
@@ -44,16 +49,19 @@ class CreateRoomView(APIView):
             votes_to_skip = serializer.data.get('votes_to_skip')
             host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
+            # If the host already has a room, update it with the new data
             if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            # If the host does not have a room, create a new one
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
+        # If the data provided is invalid, return an error message
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
